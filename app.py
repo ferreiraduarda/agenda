dd206ca197d0db92a3b64a669a08c5bedaaa1c5b
ferreiraduarda <maria.e.santos432@aluno.senai.br>
2026-03-02 10:23:04 -0300

    build: limpeza do projeto para deploy

--- app.py
+++ app.py
@@ -6,36 +6,34 @@ app = Flask(__name__)
 
 init_db()
 
-@app.route('/')
-def home():
-    return render_template('home.html', titulo='Home')
 
-@app.route('/agenda', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def agenda():
     tarefa = None
-    
-    if request.method == 'POST':
-        titulo_tarefa = request.form['titulo-tarefa']
-        data_conclusao = request.form['data-conclusao']
+
+    if request.method == "POST":
+        titulo_tarefa = request.form["titulo-tarefa"]
+        data_conclusao = request.form["data-conclusao"]
         tarefa = Tarefa(titulo_tarefa, data_conclusao)
         tarefa.salvar_tarefa()
 
     tarefas = Tarefa.obter_tarefas()
-    return render_template('agenda.html', titulo='Agenda', tarefas=tarefas)
+    return render_template("agenda.html", titulo="Agenda", tarefas=tarefas)
 
-@app.route('/delete/<int:idTarefa>')
+
+@app.route("/delete/<int:idTarefa>")
 def delete(idTarefa):
     tarefa = Tarefa.id(idTarefa)
     tarefa.excluir_tarefa()
-    # return render_template('agenda.html', titulo="Agenda", tarefas=tarefas)
-    return redirect(url_for('agenda'))
+    return redirect(url_for("agenda"))
 
-@app.route('/update/<int:idTarefa>', methods=['GET', 'POST'])
+
+@app.route("/update/<int:idTarefa>", methods=["GET", "POST"])
 def update(idTarefa):
 
-    if request.method == 'POST':
-        titulo = request.form['titulo-tarefa']
-        data = request.form['data-conclusao']
+    if request.method == "POST":
+        titulo = request.form["titulo-tarefa"]
+        data = request.form["data-conclusao"]
 
         if data == "":
             data = None
@@ -43,23 +41,21 @@ def update(idTarefa):
         tarefa = Tarefa(titulo, data, idTarefa)
         tarefa.atualizar_tarefa()
 
-        return redirect(url_for('agenda'))
+        return redirect(url_for("agenda"))
 
     tarefas = Tarefa.obter_tarefas()
     tarefa_selecionada = Tarefa.id(idTarefa)
 
     return render_template(
-        'agenda.html',
-        titulo=f'Editando a tarefa ID: {idTarefa}',
+        "agenda.html",
+        titulo=f"Editando a tarefa ID: {idTarefa}",
         tarefas=tarefas,
-        tarefa_selecionada=tarefa_selecionada)
+        tarefa_selecionada=tarefa_selecionada,
+    )
+
 
 @app.route("/tarefas/<int:id>/status", methods=["POST"])
 def alterar_status(id):
     tarefa = Tarefa.id(id)
     tarefa.alterar_status()
     return redirect(url_for("agenda"))
-
-@app.route('/ola')
-def ola_mundo():
-    return "Olá, Mundo!"
--- models/tarefa.py
+++ models/tarefa.py
@@ -6,16 +6,16 @@ from datetime import datetime
 
 class Tarefa:
     """
-        Classe para representar uma tarefa, com métodos para salvar, obter, excluir e atualizar tarefas em um banco de dados usando a classe `Database`.
+        Classe para representar uma tarefa, com métodos para salvar, obter, 
+        excluir e atualizar tarefas em um banco de dados usando a classe 
+        `Database`.
     """
-    def __init__(self: Self, titulo_tarefa: Optional[str], data_conclusao: Optional[str] = None, id_tarefa: Optional[int] = None) -> None:
+    def __init__(self: Self, titulo_tarefa: Optional[str], data_conclusao: 
+                Optional[str] = None, id_tarefa: Optional[int] = None) -> None:
         self.titulo_tarefa: Optional[str] = titulo_tarefa
         self.data_conclusao: Optional[str] = data_conclusao
         self.id_tarefa: Optional[int] = id_tarefa
 
-    # Tarefa(titulo_tarefa="Nova tarefa")
-    # Tarefa(titulo_tarefa="Outra tarefa", data_conclusao="2026-02-03")
-    # Tarefa(id-1)
     @property
     def concluida(self) -> bool:
         return self.data_conclusao is not None
@@ -23,23 +23,18 @@ class Tarefa:
     @classmethod
     def id(cls, id: int) -> Self:
         with Database() as db:
-            query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas WHERE id = ?;'
+            query: str = """
+            SELECT titulo_tarefa, data_conclusao FROM tarefas WHERE id = ?;
+            """
             params: tuple = (id,)
             resultado: list[Any] = db.buscar_tudo(query, params)
-            # Desempacotamento de coleção
 
             [[titulo, data]] = resultado
 
         return cls(id_tarefa=id, titulo_tarefa=titulo, data_conclusao=data)
 
-    # Simulando o conceito de sobrecarga
-    # Tarefa('Título da tarefa')
-    # Tarefa('Título da tarefa', '2026-02-03')
-    # Tarefa.id(1)
-
-
     def salvar_tarefa(self) -> None:
-        if self.data_conclusao == "":
+        if not self.data_conclusao:
             self.data_conclusao = None
 
         with Database() as db:
@@ -53,9 +48,12 @@ class Tarefa:
     @classmethod
     def obter_tarefas(cls):
         with Database() as db:
-            query = 'SELECT titulo_tarefa, data_conclusao, id FROM tarefas;'
+            query = """
+            SELECT titulo_tarefa, data_conclusao, id FROM tarefas;
+            """
             resultados: list[Any] = db.buscar_tudo(query)
-            tarefas: list[Self] = [cls(titulo, data, id) for titulo, data, id in resultados]
+            tarefas: list[Self] = [cls(titulo, data, id) 
+                                   for titulo, data, id in resultados]
             return tarefas
         
     def excluir_tarefa(self) -> Cursor:
@@ -66,7 +64,8 @@ class Tarefa:
             return resultado
         
     def atualizar_tarefa(self) -> Cursor:
-        if self.data_conclusao == "":
+
+        if not self.data_conclusao:
             self.data_conclusao = None
 
         with Database() as db:
@@ -75,20 +74,23 @@ class Tarefa:
             SET titulo_tarefa = ?, data_conclusao = ?
             WHERE id = ?;
             """
+
             params: tuple = (
                 self.titulo_tarefa,
                 self.data_conclusao,
                 self.id_tarefa
             )
 
-        resultado: Cursor = db.executar(query, params)
+            resultado: Cursor = db.executar(query, params)
 
         return resultado
         
     def alterar_status(self) -> Cursor:
 
         with Database() as db:
-            query_verificar = "SELECT data_conclusao FROM tarefas WHERE id = ?;"
+            query_verificar = """
+            SELECT data_conclusao FROM tarefas WHERE id = ?;
+            """
             resultado = db.buscar_tudo(query_verificar, (self.id_tarefa,))
 
             data = resultado[0][0]
